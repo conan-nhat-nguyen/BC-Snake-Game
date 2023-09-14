9bbb5028825db51466859b2bc616b738108f4506
Nhat Nguyen (Conan) <111167849+conan-nhat-nguyen@users.noreply.github.com>
2023-09-13 22:08:02 -0500

    Buttons properties initialized and main skeleton set up

--- BCSnakeMenu/src/menu/MainMenu.java
+++ BCSnakeMenu/src/menu/MainMenu.java
@@ -1,7 +1,36 @@
 package menu;
 
-public class MainMenu {
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+
+public class MainMenu extends MouseAdapter {
+	
+	boolean active;
+	
+	// Button "Play"
+	private Rectangle playButton;
+	String textPlay = "Play";
+	boolean pHighlighted = false;
+	
+	// Button "Settings"
+	private Rectangle settingsButton;
+	String textSettings = "Settings";
+	boolean sHighlighted = false;
+	
+	// Button "Scoreboard"
+	private Rectangle scoreboardButton;
+	String textScoreboard = "Scoreboard";
+	boolean sbHighlighted = false;
+	
+	// Button "Exit"
+	private Rectangle exitButton;
+	String textExit = "Exit";
+	boolean eHighlighted = false;
+	
 	public static void main(String args[]) {
 		System.out.println("Hello World");
+		
+
+		
 	}
 }
