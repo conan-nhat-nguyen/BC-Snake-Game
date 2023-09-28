bb9ac59df0c4980adb33f6d945c9feafdc78f02b
baodao2002 <114450898+baodao2002@users.noreply.github.com>
2023-09-27 21:24:07 -0700

    Merge branch 'main' of https://github.com/conan-nhat-nguyen/BC-Snake-Game.git into main

---
+++ BCSnakeGame/src/SnakeGame/FrameScreen.java
@@ -0,0 +1,23 @@
+package SnakeGame;
+import javax.swing.JFrame;
+
+public class FrameScreen extends JFrame {
+	MainGame mainGame;
+	
+	public FrameScreen() {
+		mainGame = new MainGame();
+		add (mainGame);
+
+		
+		
+	}
+	public static void main(String as[]) {
+		FrameScreen f = new FrameScreen();
+		f.setVisible(true);
+		f.setSize(500,500);
+		
+		
+		
+	}
+
+}
