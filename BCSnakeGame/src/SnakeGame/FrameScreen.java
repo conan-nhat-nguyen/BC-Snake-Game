c407c0d0456219fd6befb1bb42dab31e7649d8b7
Nhat Nguyen (Conan) <111167849+conan-nhat-nguyen@users.noreply.github.com>
2023-09-27 23:42:21 -0500

    test test

--- BCSnakeGame/src/SnakeGame/FrameScreen.java
+++ BCSnakeGame/src/SnakeGame/FrameScreen.java
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-package SnakeGame;
-import javax.swing.JFrame;
-
-public class FrameScreen extends JFrame {
-	MainGame mainGame;
-	
-	public FrameScreen() {
-		mainGame = new MainGame();
-		add (mainGame);
-
-		
-		
-	}
-	public static void main(String as[]) {
-		FrameScreen f = new FrameScreen();
-		f.setVisible(true);
-		f.setSize(500,500);
-		
-		
-		
-	}
-
-}
-=======
 package SnakeGame;
 import javax.swing.JFrame;
 
