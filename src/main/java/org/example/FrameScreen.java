0403187296f729dfd16869d1aafac72c709e3737
Nhat Nguyen (Conan) <111167849+conan-nhat-nguyen@users.noreply.github.com>
2023-10-06 20:36:28 -0500

    Reset Directory to have only BCSnakeGame

---
+++ src/main/java/org/example/FrameScreen.java
@@ -0,0 +1,22 @@
+package org.example;
+import javax.swing.JFrame;
+
+public class FrameScreen extends JFrame {
+    MainGame mainGame;
+
+    public FrameScreen() {
+        mainGame = new MainGame();
+        add(mainGame);
+
+
+
+    }
+    public static void main(String as[]) {
+        FrameScreen f = new FrameScreen();
+        f.setVisible(true);
+        f.setSize(500,500);
+
+    }
+
+}
+
---
+++ src/main/java/org/example/MainGame.java
@@ -0,0 +1,53 @@
+package org.example;
+import java.awt.*;
+import javax.swing.*;
+
+public class MainGame extends JPanel implements Runnable {
+
+    Thread thread;
+    int [][] bg = new int [20][30];
+    int step = 0;
+    theSnake snake;
+    public MainGame() {
+        snake = new theSnake();
+        thread = new Thread(this);
+        thread.start();
+        bg[5][6] = 1;
+        bg[4][7] = 2;
+
+    }
+    public void run() {
+        // TODO Auto-generated method stub
+        while(true){
+            snake.update();
+            repaint();
+            try {
+                thread.sleep(60);
+
+            } catch (InterruptedException ex) {
+                // TODO: handle exception
+            }
+        }
+
+    }
+
+    public void paintBg(Graphics g) {
+        g.setColor(Color.GRAY);
+        for(int i = 0; i < 30; i++) {
+            for( int j = 0; j < 20; j++) {
+                g.fillRect(i*20+1, j*20+1, 18, 18);
+
+
+            }
+        }
+    }
+
+
+
+    public void paint(Graphics g) {
+        paintBg(g);
+        snake.drawSnake(g);
+    }
+
+}
+
