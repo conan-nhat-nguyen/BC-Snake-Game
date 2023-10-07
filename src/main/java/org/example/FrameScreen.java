646a944e149d1f3f8aadc0b739d9a1cf8a7652d6
baodao2002 <114450898+baodao2002@users.noreply.github.com>
2023-10-06 20:45:11 -0700

    hi

--- src/main/java/org/example/FrameScreen.java
+++ src/main/java/org/example/FrameScreen.java
@@ -3,17 +3,10 @@ import javax.swing.*;
 
 public class FrameScreen extends JFrame {
     MainGame mainGame;
-    MainMenu mainMenu;
-    public static final int WIDTH = 500;
-    public static final int HEIGHT = 500;
 
     public FrameScreen() {
         mainGame = new MainGame();
-        mainMenu = new MainMenu(mainGame);
-
-        // TODO: add the mainMenu object to FrameScreen
         add(mainGame);
-
 
 
 
@@ -21,7 +14,7 @@ public class FrameScreen extends JFrame {
     public static void main(String as[]) {
         FrameScreen f = new FrameScreen();
         f.setVisible(true);
-        f.setSize(WIDTH,HEIGHT);
+        f.setSize(500,500);
 
     }
 
--- src/main/java/org/example/MainGame.java
+++ src/main/java/org/example/MainGame.java
@@ -14,8 +14,6 @@ public class MainGame extends JPanel implements Runnable {
         bg[10][10]=2;
         thread = new Thread(this);
         thread.start();
-//        bg[5][6] = 1;
-//        bg[4][7] = 2; 
 
     }
     public void run() {
@@ -39,7 +37,7 @@ public class MainGame extends JPanel implements Runnable {
             for( int j = 0; j < 20; j++) {
                 g.fillRect(i*20+1, j*20+1, 18, 18);
                 if(bg[i][j]==2) {
-                	                	g.setColor(Color.red);
+                	g.setColor(Color.red);
                     g.fillRect(i*20+1, j*20+1, 18, 18);
                 	g.setColor(Color.gray);
 //                	
